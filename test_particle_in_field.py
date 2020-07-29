53a11605f6e8bbc62348012753a058675c2d4e4d
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-07-29 14:06:24 -0400

    Fix problems and delete .DS_Store

--- test_particle_in_field.py
+++ test_particle_in_field.py
@@ -1,37 +1,40 @@
+"""Tests the EMWave and ChargedParticle PhysicsModules"""
+
 import pytest
 from turbopy import Simulation
-from particle_in_field import EMWave, ChargedParticle, ForwardEuler
+from particle_in_field import EMWave, ChargedParticle
 
-owner_data = {"N": 10, "min": 20, "max": 30, "type": "test_pusher"}
 wave_data = {"amplitude": 10, "omega": 18}
-particle_data = {"position": 26.12, "pusher": "test_pusher"}
+particle_data = {"position": 25, "pusher": "ForwardEuler"}
 
 
-@pytest.fixture(name="EM_example")
-def fixture_EM():
-    Owner = Simulation({"Grid": owner_data})
-    Owner.read_grid_from_input()
-    return EMWave(Owner, wave_data)
+@pytest.fixture(name="sim_example")
+def fixture():
+    """"Creates a Simulation object to be used as the owner of an
+    EMWave object and a ChargedParticle PhysicsModule for testing"""
+    input_data = {"Grid": {"N": 10, "min": 20, "max": 30},
+                  "PhysicsModules": {"EMWave": wave_data,
+                                     "ChargedParticle": particle_data},
+                  "Tools": {"ForwardEuler": {}},
+                  "Clock": {"start_time": 0, "end_time": 1, "num_steps": 2}}
+    sim = Simulation(input_data)
+    sim.prepare_simulation()
+    return sim
 
 
-def test_EMWave(EM_example):
-    assert isinstance(EM_example, EMWave)
-    assert EM_example.c == 2.998e8
-    assert EM_example.E0 == wave_data["amplitude"]
-    assert EM_example.omega == wave_data["omega"]
-    assert EM_example.k == wave_data["omega"] / 2.998e8
+def test_emwave(sim_example):
+    """Tests the EMWave PhysicsModule"""
+    em_example = EMWave(sim_example, wave_data)
+    assert isinstance(em_example, EMWave)
+    assert em_example.c == 2.998e8
+    assert em_example.E0 == wave_data["amplitude"]
+    assert em_example.omega == wave_data["omega"]
+    assert em_example.k == wave_data["omega"] / 2.998e8
 
 
-@pytest.fixture(name="Charged_example")
-def fixture_Charged():
-    Owner = Simulation({"Grid": owner_data})
-    Owner.compute_tools.append(ForwardEuler(Owner, owner_data))
-    Owner.read_grid_from_input()
-    return ChargedParticle(Owner, particle_data)
-
-
-def test_ChargedParticle(Charged_example):
-    assert isinstance(Charged_example, ChargedParticle)
-    assert Charged_example.E is None
-    assert Charged_example.x == particle_data["position"]
-
+def test_chargedparticle(sim_example):
+    """Tests the ChargedParticle PhysicsModule"""
+    charged_example = ChargedParticle(sim_example, particle_data)
+    assert isinstance(charged_example, ChargedParticle)
+    assert charged_example.E is None
+    assert charged_example.x == particle_data["position"]
